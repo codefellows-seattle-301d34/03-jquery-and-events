9140f075b52b412e7cc114a7a00af587c1af522d
GoldbeardSea <timbusch1@gmail.com>
2018-05-10 11:54:45 -0700

    instituted read less read on functionality, hide pictues as well

--- gb-tracy/scripts/articleView.js
+++ gb-tracy/scripts/articleView.js
@@ -48,6 +48,8 @@ articleView.handleAuthorFilter = function() {
       $('article[class="template"]').hide();
     }
     $('#category-filter').val('');
+    $('.article-body *:nth-of-type(n+2)').hide();
+    $('.article-body img').hide();
   });
 };
 
@@ -69,7 +71,7 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
   $('.main-nav').on('click', 'li', function() {
@@ -87,9 +89,24 @@ articleView.handleMainNav = function() {
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
+  $('.article-body img').hide();
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  $('#articles').on('click', 'a', function(e) {
+    e.preventDefault();
+    if ($(this).hasClass('read-on')) {
+      ($(this).prev().children().fadeIn(500))
+      $(this).prev().children().children().fadeIn(500);
+      $(this).removeClass('read-on').addClass('read-less');
+      $(this).html('&larr; Read Less');
+    } else {
+      $('.article-body *:nth-of-type(n+2)').hide();
+      $('.article-body img').hide();
+      $(this).html('Read On &rarr;');
+      $(this).removeClass('read-less').addClass('read-on');
+    }
+  })
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -98,4 +115,5 @@ $(document).ready(function() {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
