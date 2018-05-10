7fc64a1d8e124da5c301bb4b90e0f00dc3df733b
GoldbeardSea <timbusch1@gmail.com>
2018-05-10 10:34:49 -0700

    feature of tab browsing now functional

--- gb-tracy/scripts/articleView.js
+++ gb-tracy/scripts/articleView.js
@@ -52,7 +52,7 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
@@ -72,6 +72,13 @@ articleView.handleMainNav = function() {
   // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
+  $('.main-nav').on('click', 'li', function() {
+    let clickNav = $(this).data('content');
+    if (clickNav) {
+      $('.tab-content').hide();
+      $(`#${clickNav}`).fadeIn(1000);
+    }
+  })
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('.main-nav .tab:first').click();
@@ -90,4 +97,5 @@ $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 })
