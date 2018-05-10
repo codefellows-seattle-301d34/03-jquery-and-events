b68ff9dc03d0c2eecbfb24d4add36a56bd920fd1
Jermaine Walker <jermainemwalker@gmail.com>
2018-05-10 12:53:29 -0700

    Read on is working.

--- jermaine-amy/scripts/articleView.js
+++ jermaine-amy/scripts/articleView.js
@@ -54,7 +54,6 @@ articleView.handleAuthorFilter = function () {
       // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $( 'article' ).fadeIn( 750 );
       $( 'article.template' ).hide();
-
     }
     $( '#category-filter' ).val( '' );
   } );
@@ -74,12 +73,11 @@ articleView.handleCategoryFilter = function () {
       $( 'article.template' ).hide();
     }
     $( '#author-filter' ).val( '' );  
-    
   } )
 };
 
 articleView.handleMainNav = function () {
-  // TODO: Add an event handler to nav elements that will power the tabs feature.
+  // DONE: Add an event handler to nav elements that will power the tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So, you need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
@@ -100,7 +98,19 @@ articleView.setTeasers = function () {
   $( '.article-body *:nth-of-type(n+2)' ).hide();
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
-  // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words...delegate) any .read-on clicks that happen within child nodes.
+  // PSEUDO CODE 
+  // 1. Create an event listener on the #article ID.
+  // 2. Create an event handler which recognizes .on-read click.
+  // 3. Show the rest of .article-body class
+  $( '#articles' ).on( 'click', '.read-on', function ( event ) {
+    event.preventDefault();
+    $( this ).parent().find('*').show();
+
+  }  )
+
+
+
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -110,4 +120,6 @@ $( document ).ready( function () {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
+
 } )
