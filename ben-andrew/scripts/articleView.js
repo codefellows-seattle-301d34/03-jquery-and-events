6a890d95ae783b34af2ed4062e2bd43e66cd0fcb
Andrew Peacock <todoville@gmail.com>
2018-05-10 11:27:27 -0700

    finished!

--- ben-andrew/scripts/articleView.js
+++ ben-andrew/scripts/articleView.js
@@ -86,7 +86,14 @@ $('nav li').on('click', function(){
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
+  $('.read-on').on('click', function(e) {
+    e.preventDefault();
+    let $article = $(this).parent();
+    $article.find('.article-body *:nth-of-type(n+2)').fadeIn();
+    $(this).hide();
+    console.log($article.find('.article-body'));
 
+  })
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
@@ -97,4 +104,5 @@ $(document).ready(function() {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
