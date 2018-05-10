a5649903047bf44d911d5dfd5195c563bd5be755
ria agnes jose <joseagnesria@gmail.com>
2018-05-10 09:54:35 -0700

    added template literal

--- ria-ashton/scripts/articleView.js
+++ ria-ashton/scripts/articleView.js
@@ -1,5 +1,5 @@
 'use strict';
-
+alert('hi');
 // REVIEW: Configure an object to hold all of our functions for dynamic updates and article-related event handlers.
 let articleView = {};
 
@@ -14,9 +14,10 @@ articleView.populateFilters = function() {
       authorName = $(this).attr('data-author');
 
       // TODO: Refactor this concatenation using a template literal.
-      optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
-
-      if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
+      optionTag = `<option value="${authorName}">${authorName} </option>`;
+  
+      
+      if ($(`#author-filter option[value="${authorName}"]`).length === 0) {
         $('#author-filter').append(optionTag);
       }
 
@@ -25,9 +26,9 @@ articleView.populateFilters = function() {
       category = $(this).attr('data-category');
 
       // TODO: Refactor this concatenation using a template literal.
-      optionTag = '<option value="' + category + '">' + category + '</option>';
+      optionTag = `<option value="${category}">${category}</option>`;
 
-      if ($('#category-filter option[value="' + category + '"]').length === 0) {
+      if ($(`#category-filter option[value="${category}"]`).length === 0) {
         $('#category-filter').append(optionTag);
       }
     }
@@ -38,6 +39,8 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
+
+      $('article').find(`article[data-author="${this.author}"]`)
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
